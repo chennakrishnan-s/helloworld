8365c056c75c39de7794f3f2a2092ca0f4e6f917
chennakrishnan s <144014025+chennakrishnan-s@users.noreply.github.com>
2024-04-23 18:53:46 +0530

    Add files via upload

---
+++ udemy.py
@@ -0,0 +1,12 @@
+# import heroes
+# for _ in range(3):
+#     print(heroes.gen())
+import turtle as t
+
+tim = t.Turtle()
+
+for _ in range(13):
+    tim. forward(10)
+    tim.penup()
+    tim. forward(10)
+    tim.pendown()
